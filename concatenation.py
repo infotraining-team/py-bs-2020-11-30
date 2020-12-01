e553ad2cf96ee3ee3b84a26eb8a039d958ec2bd9
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2020-12-01 14:58:18 +0100

    clean

--- concatenation.py
+++ concatenation.py
@@ -2,10 +2,24 @@
 
 book = open("proust.txt", 'r', encoding='UTF8').read()
 words = book.split()
+words_clean = []
 
-for word in words:
+#clean-up
+for dirty in words:
+    clean = dirty.strip('.?!();:\t\n')
+    if clean.endswith("'s"):
+        clean = clean[:-2]
+    words_clean.append(clean.lower())
+
+print(words_clean[:1000])
+
+words_freq = {"the" : 734, "swann" : 234}
+
+for word in words_clean:
     pass
 
 # divide to words (white chars)
 # count the same words! - use dictionary
+
+# -- later -- :)
 # sort and print
