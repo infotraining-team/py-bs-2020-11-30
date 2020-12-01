724ba7f7333dcf8ceb07bdad0d3f2286354e3225
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2020-12-01 15:59:51 +0100

    functions - multiple arguments

--- functions.py
+++ functions.py
@@ -21,3 +21,24 @@ print_person(name=imie, surname=nazwisko)
 print_person(surname=nazwisko, name="Leszek")
 print_person("Oleg")
 help(add)
+
+print("---------")
+def multiadd(*args):
+    res = 0
+    for i in args:
+        res += i
+    return res
+
+print(multiadd(2, 3))
+print("---------")
+print(multiadd(2, 3, 4, 5, 6))
+
+def print_names(name, surname, *args, **kwargs):
+    print(name, surname)
+    print(args)
+    print(kwargs)
+
+print_names("Leszek", "T", 213)
+print_names(name="Leszek", surname="T", age=43)
+
+
