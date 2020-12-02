669c94b00c3e710adaec9e4d68c801786d7e816b
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2020-12-02 15:03:53 +0100

    day3 - fibo

--- fibo.py
+++ fibo.py
@@ -2,7 +2,14 @@ import unittest
 
 def fibo(n):
     """returns first n elements of Fibbonaci sequence"""
-    result = [1]
+    if n < 1:
+        raise TypeError
+    if n == 1:
+        result = [1]
+    if n > 1:
+        result = [1,1]
+        for i in range(n-2):
+            result.append(result[-1] + result[-2])
     return result
 
 # [1, 1, 2, 3, 5, 8, 13]
@@ -15,8 +22,17 @@ class FiboTest(unittest.TestCase):
         self.assertEqual(fibo(1), [1])
 
     ## Test second element 2 -> 1, 1
+    def test_return_two_ones_for_two(self):
+        self.assertEqual(fibo(2), [1, 1])
+        
     ## Test 7 -> [1, 1, 2, 3, 5, 8, 13]
+    def test_return_first_seven_elements(self):
+        self.assertEqual(fibo(7), [1, 1, 2, 3, 5, 8, 13])
+
     ## Test input TypeError for negative number
+    def test_raises_when_negative(self):
+        with self.assertRaises(TypeError):
+            fibo(-1)
 
 if __name__ == "__main__":
     unittest.main()
